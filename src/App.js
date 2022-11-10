d032bd91a0be807c751d436a616743397ba68640
mahmutcopoglu <mahmutcopoglu@gmail.com>
2022-11-11 00:46:24 +0300

    main

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import "primereact/resources/themes/lara-light-indigo/theme.css";  
 import "primereact/resources/themes/tailwind-light/theme.css";                  
 import "primeicons/primeicons.css";   
 import React, { useState } from 'react';
-import { InputText } from 'primereact/inputtext';
-import { Button } from 'primereact/button';
-import { Knob } from 'primereact/knob';
+
 function App() {
-  const [value, setValue] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
